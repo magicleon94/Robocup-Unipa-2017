68d912eea69fe43998151e641f73ec924ca62471
vincenzo-vd <vincenzo.degregorio001@gmail.com>
2017-05-18 09:30:28 +0200

    Server
    The script now accept a JSON and send a string containing an integer,
    that indicates the robot speed

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-#this script accept a vector from arduino and send
-#a json to it
+#this script accept a JSON, as a string, and send a string that contains an integer
+#that indicates the speed
+
 import json
 import socket
-TCP_IP = "192.168.1.101"
+TCP_IP = "localhost"
 TCP_PORT = 1931
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # IP .4 & TCP
 s.bind((TCP_IP, TCP_PORT)) #bind socket
 
-BUFFER_SIZE = 96  #  PENSO POTREBBE ESSERE ANCHE PIU' PICCOLO
-
+BUFFER_SIZE = 100  #  BUFFER SIZE - da controllare se aumentare o diminuire
 # This function takes an int argument called backlog, which specifies the
 # maximum number of  connections that are kept waiting if the application is
 # already busy.
@@ -24,30 +24,40 @@ while True: # wait connection
         print "There is no message"
         break
 
+    input_dictionary = json.loads(message)
 
-    #input_vector = [left_sensor, central_sensor, right_sensor]
-    input_vector = eval(message)
+    # FORWARD           0
+    # FORWARD_FAST      1
+    # BACKWARD          2
+    # TURN_LEFT         3
+    # TURN_LEFT_MICRO   4
+    # TURN_RIGHT        5
+    # TURN_RIGHT_MICRO  6
+    # GRAB              10
+    # RELEASE           11
+    # BACKWARD_LEFT     12
+    # BACKWARD_RIGHT    13
 
 
-    if input_vector == [1, 1, 1]:
-        data = {"left_engine":80,"right_engine":80,"time":200}
-    elif input_vector == [0, 1, 1]:
-        data = {"left_engine":80,"right_engine":0,"time":200}
-    elif input_vector == [1, 1, 0]:
-        data = {"left_engine":0,"right_engine":80,"time":200}
-    elif input_vector == [1, 0, 1]:
-        data = {"left_engine":80,"right_engine":0,"time":200}
-    elif input_vector == [0, 1, 0]:
-        data = {"left_engine":80,"right_engine":80,"time":100}
-    elif input_vector == [0, 0, 1]:
-        data = {"left_engine":0,"right_engine":-80,"time":1000}
-    elif input_vector == [1, 0, 0]:
-        data = {"left_engine":-80,"right_engine":0,"time":1000}
-    elif input_vector == [0, 0, 0]:
-        data = {"left_engine":-80,"right_engine":-80,"time":1500}
+    if input_dictionary["leftObstacle"] == 1 and input_dictionary["frontObstacle"] == 1 and input_dictionary["rightObstacle"] == 1:
+        server_message = 1
+    elif input_dictionary["leftObstacle"] == 0 and input_dictionary["frontObstacle"] == 1 and input_dictionary["rightObstacle"] == 1:
+        server_message = 6
+    elif input_dictionary["leftObstacle"] == 1 and input_dictionary["frontObstacle"] == 1 and input_dictionary["rightObstacle"] == 0 :
+        server_message = 4
+    elif input_dictionary["leftObstacle"] == 1 and input_dictionary["frontObstacle"] == 0 and input_dictionary["rightObstacle"] == 1:
+        server_message = 6
+    elif input_dictionary["leftObstacle"] == 0 and input_dictionary["frontObstacle"] == 1 and input_dictionary["rightObstacle"] == 0:
+        server_message = 1
+    elif input_dictionary["leftObstacle"] == 0 and input_dictionary["frontObstacle"] == 0 and input_dictionary["rightObstacle"] == 1:
+        server_message = 12
+    elif input_dictionary["leftObstacle"] == 1 and input_dictionary["frontObstacle"] == 0 and input_dictionary["rightObstacle"] == 0:
+        server_message = 13
+    else :
+        server_message = 2
 
     #the message sent from the server must be a string or buffer
-    server_message = str(json.dumps(data))
+    server_message = str(server_message)
 
     conn.send(server_message)
 
