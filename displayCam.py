006778dde4419be5c40eb8cbd01b192647db6577
magicleon <magicleon94@gmail.com>
2017-06-02 13:16:02 +0200

    Sample

---
+++ displayCam.py
@@ -0,0 +1,18 @@
+import cv2
+import imutils
+cap = cv2.VideoCapture('rtsp://@192.168.1.1/live/ch00_0', cv2.CAP_FFMPEG)
+while(True):
+    #frame = cv2.imread('immagini/50cm.jpg', 1)
+    ret, frame = cap.read()
+    frame = imutils.resize(frame, width=600)
+    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+
+    cv2.imshow('img', frame)
+    #cv2.waitKey(0)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    #cv2.destroyAllWindows()
+
+#cap.release()
+cv2.destroyAllWindows()
+#kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(7,7))
