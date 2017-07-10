69b8d725ced2a07a095d84737b8f03e89cedcd75
magicleon <magicleon94@gmail.com>
2017-07-10 17:30:48 +0200

    Comportamento del robot generalizzato

---
+++ robot.py
@@ -0,0 +1,157 @@
+import json
+import socket
+import constants
+import cv2
+import threading
+from DetectorHandler import DetectorHandler
+import time
+from States import States
+
+# globals
+running = True
+cap = cv2.VideoCapture('rtsp://@192.168.1.101/live/ch00_0', cv2.CAP_FFMPEG)
+ret = None
+
+# networking parameters
+TCP_IP = "192.168.1.234"
+TCP_PORT = 1931
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((TCP_IP, TCP_PORT))  # bind socket
+BUFFER_SIZE = 512
+
+
+class FramesGrabber(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global cap
+        global ret
+        global running
+        while running:
+            ret = cap.grab()
+
+
+def reactive(leftObstacle, rightObstacle, frontObstacle):
+    print "Reactive"
+    if not leftObstacle and not rightObstacle and not frontObstacle:
+        conn.send(str(constants.FORWARD))
+    elif leftObstacle and rightObstacle:
+        conn.send(str(constants.BACKWARD))
+    elif leftObstacle:
+        conn.send(str(constants.TURN_RIGHT))
+    elif rightObstacle:
+        conn.send(str(constants.TURN_LEFT))
+    else:
+        conn.send(str(constants.BACKWARD_LEFT))
+
+
+def reorient(somethingAtRight, rightObstacle, somethingAtLeft, leftObstacle, toTurn):
+    print "Reorienting"
+    print "ToTurn: ", toTurn
+    print "Left: ", leftObstacle
+    print "Right: ", rightObstacle
+
+    # se non ho nulla alla mia destra ha senso orientarmi ora
+    if not somethingAtRight:
+        if toTurn > 5.0 and not rightObstacle:
+            if toTurn < 20.0:
+                conn.send(str(constants.TURN_RIGHT_MICRO))
+                return True
+            else:
+                conn.send(str(constants.TURN_RIGHT))
+                return True
+    # se non ho nulla alla mia sinistra ha senso orientarmi ora
+    if not somethingAtLeft:
+        if toTurn < -5.0 and not leftObstacle:
+            if toTurn < -20.0:
+                conn.send(str(constants.TURN_LEFT_MICRO))
+                return True
+
+            else:
+                conn.send(str(constants.TURN_LEFT))
+                return True
+    print "Not reoriented"
+    return False
+
+
+frames_grabber = FramesGrabber()
+detector_handler = DetectorHandler()
+states_manager = States()
+
+following = False
+
+frames_grabber.start()
+print "Grabbing frames started"
+s.listen(1)
+print "Listening started"
+try:
+    while True:
+        conn, addr = s.accept()
+        message = conn.recv(BUFFER_SIZE)
+
+        print message, '\n'
+
+        input_dictionary = json.loads(message)
+
+        # IR data
+        leftObstacle = input_dictionary["leftObstacle"] == 0
+        frontObstacle = input_dictionary["frontObstacle"] == 0
+        rightObstacle = input_dictionary["rightObstacle"] == 0
+
+        # Sonar data
+        somethingAtLeft = 0 < input_dictionary['leftDistance'] < 10
+        somethingAtRight = 0 < input_dictionary['rightDistance'] < 10
+
+        # Compass data
+        currentDegrees = input_dictionary['degrees']
+
+        targetDegrees, targetType, targetColor = states_manager.get_targets()
+        if target_color == "green":
+            toTurn = 0
+        else:
+            toTurn = targetDegrees - currentDegrees
+
+        ret, frame = cap.retrieve()
+
+        if frame is not None:
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            detector_handler.find_target(
+                frame, color=targetColor, type_obj=targetType)
+            print "Showing frame"
+            # se trovi un target vai al target
+            if detector_handler.target is not None:
+                if not (leftObstacle or frontObstacle or rightObstacle):
+                    following = True
+                    conn.send(str(detector_handler.do_action()))
+                    print "Going to object"
+                    continue
+
+            # no proper target found, adjust the orientation
+            if reorient(somethingAtRight, rightObstacle, somethingAtLeft, leftObstacle, toTurn):
+                # se stavo seguendo aggiorna l'obiettivo
+                if following:
+                    states_manager.state_transition()
+                    following = False
+                continue
+
+            reactive(leftObstacle, rightObstacle, frontObstacle)
+
+        else:
+            conn.send('0')  # send NOP
+            print "Frame is none"
+
+
+except KeyboardInterrupt:
+    print "Shutting down"
+
+running = False
+cap.release()
+print "Capture device released"
+cv2.destroyAllWindows()
+print "Windows cleared"
+s.close()
+print "Socket closed"
